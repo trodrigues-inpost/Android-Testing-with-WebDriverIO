7f3d5597e339b1d9493f53d28d17d0980cc6daec
Tiago Laim <trodrigues@inpost.pl>
2025-05-19 15:19:44 +0200

    Rename checkout test suite and integrate Selectors for improved XPath management

--- test/pageobjects/selectors.objects.ts
+++ test/pageobjects/selectors.objects.ts
@@ -1,7 +1,15 @@
+// This class will serve as a library for IDs and XPaths
 class Selectors {
     static get emptyCartTitle() { return '//android.widget.TextView[@text="Your cart is empty"]'; }
     static get yourCartTitle() { return '//android.widget.TextView[@text="YOUR CART"]'; }
     static get cartIcon() { return '//android.view.ViewGroup[@content-desc="test-Cart"]/android.view.ViewGroup/android.widget.ImageView'; }
+    // Checkout Related
+    static get checkoutButton() { return '~test-CHECKOUT'; }
+    static get checkoutContinueButton() { return '//android.view.ViewGroup[@content-desc="test-CONTINUE"]'; }
+    static get checkoutFirstName() { return '//android.widget.EditText[@content-desc="test-First Name"]'; }
+    static get checkoutLastName() { return '//android.widget.EditText[@content-desc="test-Last Name"]'; }
+    static get checkoutPostalCode() { return '//android.widget.EditText[@content-desc="test-Zip/Postal Code"]'; }
+    static get checkoutFinishButton() { return '//android.view.ViewGroup[@content-desc="test-FINISH"]'; }
 
     static itemTitle(num) {
         return `(//android.widget.TextView[@content-desc="test-Item title"])[${num}]`;
--- test/specs/checkout.spec.ts
+++ test/specs/checkout.spec.ts
@@ -1,12 +1,18 @@
 // Shopping Cart Test Suite
-import { $, $$, expect, browser } from '@wdio/globals';
+import { $, $$, expect, browser, driver } from '@wdio/globals';
 import Page from '../pageobjects/page.ts';
 import LoginPage from '../pageobjects/login.page.ts';
 import { isDisplayed, waitFE } from '../helpermethods/elements.helper.ts';
+import Selectors from '../pageobjects/selectors.objects.ts';
+import { post } from 'jquery';
 
 //! [//-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-//]
 //! [// CHECK THE README.md FILE IN THIS FOLDER, IT HAS IMPORTANT INFORMATION //] 
 //! [//-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-//]
+
+const firstName = 'Test First Name';
+const lastName = 'Test Last Name';
+const postalCode = '1234-567';
 
 describe('Shopping Cart & Checkout Integration', () => {
     var itemsInCart: string[] = [];
@@ -28,24 +34,18 @@ describe('Shopping Cart & Checkout Integration', () => {
         const randomNumber = Math.floor(Math.random() * 2) + 1;
 
         // XPath for item titles
-        const titleSelector = `(//android.widget.TextView[@content-desc="test-Item title"])[${randomNumber}]`;
-        // XPath for buttons
-        const buttonSelector = `(//android.view.ViewGroup[@content-desc="test-ADD TO CART"])[${randomNumber}]`;
-
-
-        const itemTitleElement = await $(titleSelector);
-        
+        const itemTitleElement = await $(Selectors.itemTitle(randomNumber));
         const itemName = await itemTitleElement.getText();
         itemsInCart.push(itemName);
 
         // Click the corresponding "Add to Cart" button
-        const addButton = await $(buttonSelector);
+        const addButton = await $(Selectors.addToCart(randomNumber));
         await addButton.click();
 
         itemsInCartCount++;
 
         // Confirm cart icon exists
-        const isCartIconDisplayed = await $('//android.view.ViewGroup[@content-desc="test-Cart"]/android.view.ViewGroup/android.widget.ImageView');
+        const isCartIconDisplayed = await $(Selectors.cartIcon);
 
         // Expect the cart icon to be displayed
         expect(isCartIconDisplayed).toBeDisplayed();
@@ -54,15 +54,15 @@ describe('Shopping Cart & Checkout Integration', () => {
     // Test case for checking that the added item is in the cart
     it('should display the added item in the cart', async () => {
         // Click on the cart icon
-        await $('//android.view.ViewGroup[@content-desc="test-Cart"]/android.view.ViewGroup/android.widget.ImageView').click();
+        await $(Selectors.cartIcon).click();
 
         // The page should have the title 'Your Cart'
-        const cartTitle = await $('//android.widget.TextView[@text="YOUR CART"]');
+        const cartTitle = await $(Selectors.yourCartTitle);
         const isCartTitle = await cartTitle.isDisplayed();
         expect(isCartTitle);
 
         // Check if the cart is empty
-        const isCartEmpty = await isDisplayed($('//android.widget.TextView[@text="Your cart is empty"]'));
+        const isCartEmpty = await isDisplayed($(Selectors.emptyCartTitle));
         expect(!isCartEmpty);
 
         // Check if the cart contains the added item
@@ -71,13 +71,49 @@ describe('Shopping Cart & Checkout Integration', () => {
         expect(isItemInCart);
 
         // Check if the cart count is correct
-        const cartCount = await $('//android.view.ViewGroup[@content-desc="test-Cart"]/android.view.ViewGroup/android.widget.TextView');
+        const cartCount = await $(Selectors.cartIcon);
         const cartCountText = await cartCount.getText();
         const cartCountNumber = parseInt(cartCountText, 10);
         expect(cartCountNumber === itemsInCartCount);
     });
 
     it('should navigate to the checkout screen', async () => {
+        const checkoutBtn = await $(Selectors.checkoutButton);
+
+        await checkoutBtn.click();
+
+        await expect(await $(Selectors.checkoutContinueButton)).toBeDisplayed();
+    });
+
+    it('should fill the checkout form', async () => {
+        const fieldFirstName = await $(Selectors.checkoutFirstName);
+        const fieldLastName = await $(Selectors.checkoutLastName);
+        const fieldPostalCode = await $(Selectors.checkoutPostalCode);
         
+        await $(fieldFirstName).setValue(firstName);
+        await $(fieldLastName).setValue(lastName);
+        await $(fieldPostalCode).setValue(postalCode);
+
+        // Expect to have the inserted values
+        expect(await fieldFirstName.getAttribute('text')).toBe(firstName);
+        expect(await fieldLastName.getAttribute('text')).toBe(lastName);
+        expect(await fieldPostalCode.getAttribute('text')).toBe(postalCode);
+    });
+
+    //TODO: Write what it SHOULD actually do
+    it('should continue to ...', async () => {
+        const continueBtn = await $(Selectors.checkoutContinueButton);
+
+        await continueBtn.click();
+        
+        await (driver as any).execute('mobile: scrollGesture', {
+            left: 100,         // starting X position
+            top: 100,          // starting Y position
+            width: 800,        // area width to perform the scroll
+            height: 1000,      // area height
+            direction: 'down', // direction: 'up', 'down', 'left', 'right'
+            percent: 0.9       // how much of the screen to scroll
+        });
+
     });
 });
