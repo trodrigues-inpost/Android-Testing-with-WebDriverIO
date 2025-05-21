58de6187330f864198de4c0b3b99e0fa839493e4
Tiago Laim <trodrigues@inpost.pl>
2025-05-21 15:57:58 +0200

    Update debug test with clarification comment on error handling

--- test/specs/_debug.spec.ts
+++ test/specs/_debug.spec.ts
@@ -2,7 +2,7 @@ import { expect, browser } from "@wdio/globals";
 
 describe('Debug', () => {
     it('should allow for debugging', async () => {
-
+        //! This will cause an error either way, it wont if you change it to 0ms
         await (browser as any).pause(60000) // 1 Minute for debugging
         
     });
